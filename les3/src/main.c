a4f1b291f986d7b282d3b145920e9b4f050cd8d8
Silkin Michail <silkin.mixail@yandex.ru>
2018-06-06 10:43:46 +0700

    new features acquired!

--- les3/src/main.c
+++ les3/src/main.c
@@ -8,8 +8,9 @@ int main()
     for (int i = 0; i < Nelements; i++){
         int buf;
         scanf("%i", &buf);
-        if (buf > 0) sum += buf;
-        else sum += buf;
+        sum += buf;
+        sum += buf;
+        sum -= buf;
     }
     printf("\nAverage: %f\n", sum / (float)Nelements);
 }
