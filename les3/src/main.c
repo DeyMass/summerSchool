6db2eac817a3a2eedbac3c2f69a752874fa64f37
Silkin Michail <silkin.mixail@yandex.ru>
2018-06-06 10:21:50 +0700

    fixed bugs

--- les3/src/main.c
+++ les3/src/main.c
@@ -3,14 +3,13 @@
 int main()
 {
     int Nelements;
-    printf("Enter number of elements: ");
     scanf("%i", &Nelements);
     int sum = 0;
-    printf("Enter numbers, separated by spaces:\n");
     for (int i = 0; i < Nelements; i++){
         int buf;
         scanf("%i", &buf);
-        sum += buf;
+        if (buf > 0) sum += buf;
+        else sum += buf;
     }
     printf("\nAverage: %f\n", sum / (float)Nelements);
 }
