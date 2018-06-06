9886d87a26b46da7b3bf7c5ef03e4ad896af165b
Silkin Michail <silkin.mixail@yandex.ru>
2018-06-06 10:24:13 +0700

    Revert "fixed bugs"
    This reverts commit 09417793f95d25c191e9270c1cff015ab79e38bf, reversing
    changes made to 4021d1102f15a3ee1e11220358101f9759eb1089.

--- les3/src/main.c
+++ les3/src/main.c
@@ -3,13 +3,14 @@
 int main()
 {
     int Nelements;
+    printf("Enter number of elements: ");
     scanf("%i", &Nelements);
     int sum = 0;
+    printf("Enter numbers, separated by spaces:\n");
     for (int i = 0; i < Nelements; i++){
         int buf;
         scanf("%i", &buf);
-        if (buf > 0) sum += buf;
-        else sum += buf;
+        sum += buf;
     }
     printf("\nAverage: %f\n", sum / (float)Nelements);
 }
